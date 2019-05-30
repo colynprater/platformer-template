e55917c6617ce29d04bdfdf0b6f4236146ba55d3
Colyn Prater <cprater@thefrontiergroup.com.au>
2019-05-30 22:42:51 +0800

    Create Resolver class.

--- js/utils.js
+++ js/utils.js
@@ -33,3 +33,29 @@ function playSound(sound) {
 function sound(sound) {
   return new Audio('sounds/' + sound + '.mp3');
 }
+
+function Resolver() {};
+
+Resolver.top = function(obj) {
+  return obj.y - obj.height / 2;
+}
+
+Resolver.bottom = function(obj) {
+  return obj.y + obj.height / 2;
+}
+
+Resolver.left = function(obj) {
+  return obj.x - obj.width / 2;
+}
+
+Resolver.right = function(obj) {
+  return obj.x + obj.width / 2;
+}
+
+function drawDebugDots(obj) {
+  colourRect(Resolver.left(obj), Resolver.top(obj), 2, 2, 'yellow')
+  colourRect(Resolver.right(obj), Resolver.top(obj), 2, 2, 'yellow')
+
+  colourRect(Resolver.left(obj), Resolver.bottom(obj), 2, 2, 'yellow')
+  colourRect(Resolver.right(obj), Resolver.bottom(obj), 2, 2, 'yellow')
+}
