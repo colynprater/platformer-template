2c41730909c96315565247c36566f703be343fdf
Colyn Prater <colyn.prater@moneysmart.com>
2019-06-08 14:57:14 +0800

    create update.js

---
+++ js/update.js
@@ -0,0 +1,69 @@
+function decreasePlayerMomentum() {
+  if (!player.walking && player.momentum >= 0 && player.grounded) {
+    if (player.grounded) player.momentum -= PLAYER_SLOW_RATE;
+    if (!player.grounded) player.momentum -= PLAYER_SLOW_RATE / 2;
+  }
+  if (player.momentum < 0) player.momentum = 0;
+}
+
+function movePlayer() {
+  if (player.momentum) {
+    player.x += player.facingLeft() ? player.momentum : -player.momentum;
+  }
+  if (player.jumping) {
+    player.y -= (JUMP_POWER * 1.5) - player.plummet;
+  }
+  if (!player.grounded) player.y += player.plummet;
+}
+
+function expireJump() {
+  if (player.jumping) player.jumpCountUp++;
+
+  if (player.jumpCountUp >= JUMP_POWER) {
+    player.jumping = false;
+    player.jumpCountUp = 0;
+  }
+}
+
+function increasePlayerPlummet() {
+  if (!player.grounded && player.plummet <= TERMINAL_VELOCITY) player.plummet += GRAVITY;
+}
+
+function groundPlayer() {
+  var blockStoodOn = blockAtObjBottom(player, collidables)
+  var playerFalling = !player.jumping < player.plummet;
+
+  if (blockStoodOn && playerFalling) {
+    player.y = Resolver.top(blockStoodOn) - player.height / 2;
+    player.plummet = 0;
+    player.grounded = true;
+    player.jumping = false;
+    player.jumpCountUp = 0;
+  }
+}
+
+function dropPlayer() {
+  if (!blockAtObjBottom(player, collidables)) player.grounded = false;
+}
+
+function collideHorizontally() {
+  var blockOnLeft  = blockAtObjLeft(player, collidables)
+  var blockOnRight = blockAtObjRight(player, collidables)
+
+  if (blockOnLeft) {
+    player.momentum = 0;
+    player.x = Resolver.right(blockOnLeft) + (PLAYER_WIDTH / 2) + 2
+  } else if (blockOnRight) {
+    player.momentum = 0;
+    player.x = Resolver.left(blockOnRight) - (PLAYER_WIDTH / 2) - 2
+  }
+}
+
+function collideVertically() {
+  var blockAbove = blockAtObjTop(player, collidables)
+
+  if (blockAbove) {
+    player.y = Resolver.bottom(blockAbove) + (PLAYER_HEIGHT / 2) + 2
+    player.jumping = false;
+  }
+}
